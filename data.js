c09e4508cb132ac07c0d28f4b05619eea2ad0276
Ambati, Kirankumar <kambati@webmd.net>
2019-11-17 22:40:02 +0530

    chore: complete Solution in JS

---
+++ data.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    "itemname": "idli",
+    "price": 20
+  },
+  {
+    "itemname": "vada",
+    "price": 15
+  },
+  {
+    "itemname": "rava idli",
+    "price": 35
+  },
+  {
+    "itemname": "set dosa",
+    "price": 30
+  },
+  {
+    "itemname": "ghee dosa",
+    "price": 40
+  },
+  {
+    "itemname": "pongal",
+    "price": 50
+  },
+  {
+    "itemname": "medu vada",
+    "price": 15
+  },
+  {
+    "itemname": "upma",
+    "price": 30
+  },
+  {
+    "itemname": "idiyappam",
+    "price": 10
+  },
+  {
+    "itemname": "uttapam",
+    "price": 20
+  },
+  {
+    "itemname": "puttu",
+    "price": 30
+  },
+  {
+    "itemname": "rice bath",
+    "price": 50
+  }
+];
